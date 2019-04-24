94a9e87b7ae7c3ff740b190272b3ec8fa04b9a44
Aaron Chu <aaronchu357@gmail.com>
2019-04-24 15:12:55 -0400

    Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map do |grade, names|
+      @roster[grade] = names.sort
+    end
+    @roster
+  end
+
+end
